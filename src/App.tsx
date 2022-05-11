d558f49eb45d9f4c87a843fccb282f4b47a91d0a
isaac_lee <yy0221ss@gmail.com>
2022-05-11 17:49:45 +0900

    Generic Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import './App.css';
-import { Counter } from './components/class/Counter';
+import { List } from './components/generics/List';
 
 function App() {
   return (
     <div className="App">
-      <Counter message='The count value is' />
+      {/* <List 
+      items={['I am', 'Isaac', 'Lee']} 
+      onClick={(item) => console.log(item)} 
+      />
+      <List 
+      items={[1, 2, 3]} 
+      onClick={(item) => console.log(item)} 
+      /> */}
+      <List 
+      items={[
+        {
+          id: 1,
+          first: 'Isaac',
+          lee: 'Lee',
+        },
+        {
+          id: 2,
+          first: 'Clark',
+          lee: 'Kent',
+        },
+      ]} 
+      onClick={(item) => console.log(item)} 
+      />
     </div>
   );
 }
